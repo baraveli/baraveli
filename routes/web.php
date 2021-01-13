7617e135d1581c8d04e4ecaa3d57d02cb967bfde
Mohamed jinas <j@live.mv>
2021-01-13 11:20:36 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,5 @@ Route::redirect('.git', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 Route::redirect('composer.lock', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 Route::redirect('config.py', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 Route::redirect('.htaccess', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+Route::redirect('.htpass', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 
