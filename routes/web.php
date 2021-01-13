b10104f7dd7b9deed732b134e89d4390c8c8f685
Mohamed jinas <j@live.mv>
2021-01-13 11:35:07 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,5 @@ Route::redirect('composer.lock', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 Route::redirect('config.py', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 Route::redirect('.htaccess', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 Route::redirect('.htpass', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+Route::redirect('yaameen-raaboaa-video-leaked', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 
